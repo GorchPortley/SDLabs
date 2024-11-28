efca87c6e481e42b4bf326704d993ff6f0fe2d28
GorchPortley <admin@sdlabs.cc>
2024-11-27 20:46:19 -0500

    various updates before new branch

--- resources/themes/blank/pages/dashboard/index.blade.php
+++ resources/themes/blank/pages/dashboard/index.blade.php
@@ -1,22 +1,56 @@
 <?php
     use function Laravel\Folio\{middleware, name};
-    middleware('auth');
+	middleware('auth');
     name('dashboard');
 ?>
 
 <x-layouts.app>
-  <div class="max-w-5xl lg:px-4 mx-auto">
-    <h2 class="text-2xl font-bold">Welcome, {{ auth()->user()->name }} 👋</h2>
-  </div>
-  <div class="max-w-5xl lg:px-6 mx-auto mt-8 space-y-8">
-      <div class="flex md:space-x-4 md:space-y-0 space-y-6 md:flex-row flex-col">
-          <x-app.dashboard-card icon="phosphor-user" label="Users" value="143" change-direction="up" change-percentage="25"></x-app.dashboard-card>
-          <x-app.dashboard-card icon="phosphor-money" label="Sales" value="54" change-direction="down" change-percentage="5"></x-app.dashboard-card>
-          <x-app.dashboard-card icon="phosphor-arrows-clockwise" label="Refunds" value="3" change-direction="up" change-percentage="10"></x-app.dashboard-card>
-      </div>
-      <div class="w-full p-10 space-y-4 border border-black">
-        <p>Welcome to your user dashboard. This file can be edited from your theme folder at <code class="px-2 py-1 text-xs bg-gray-100">pages/dashboard/index.blade.php</code></p>
-        <p>This is a <a href="https://livewire.laravel.com/docs/volt" target="_blank" class="underline">Laravel Volt</a>   page. Learn more about <a href="https://devdojo.com/wave/docs/pages" target="_blank" class="underline">how pages work</a> here.</p>
-      </div>
-  </div>
+	<x-app.container x-data class="lg:space-y-6" x-cloak>
+
+		<x-app.alert id="dashboard_alert" class="hidden lg:flex">This is the user dashboard where users can manage settings and access features. <a href="https://devdojo.com/wave/docs" target="_blank" class="mx-1 underline">View the docs</a> to learn more.</x-app.alert>
+
+        <x-app.heading
+                title="Dashboard"
+                description="Welcomde to an example application dashboard. Find more resources below."
+                :border="false"
+            />
+
+        <div class="flex flex-col w-full mt-6 space-y-5 md:flex-row lg:mt-0 md:space-y-0 md:space-x-5">
+            <x-app.dashboard-card
+				href="https://devdojo.com/wave/docs"
+				target="_blank"
+				title="Documentation"
+				description="Learn how to customize your app and make it shine!"
+				link_text="View The Docs"
+				image="/wave/img/docs.png"
+			/>
+			<x-app.dashboard-card
+				href="https://devdojo.com/questions"
+				target="_blank"
+				title="Ask The Community"
+				description="Share your progress and get help from other builders."
+				link_text="Ask a Question"
+				image="/wave/img/community.png"
+			/>
+        </div>
+
+		<div class="flex flex-col w-full mt-5 space-y-5 md:flex-row md:space-y-0 md:mb-0 md:space-x-5">
+			<x-app.dashboard-card
+				href="https://github.com/thedevdojo/wave"
+				target="_blank"
+				title="Github Repo"
+				description="View the source code and submit a Pull Request"
+				link_text="View on Github"
+				image="/wave/img/laptop.png"
+			/>
+			<x-app.dashboard-card
+				href="https://devdojo.com"
+				target="_blank"
+				title="Resources"
+				description="View resources that will help you build your SaaS"
+				link_text="View Resources"
+				image="/wave/img/globe.png"
+			/>
+		</div>
+    </x-app.container>
 </x-layouts.app>
