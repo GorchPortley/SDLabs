24c9e779151666b19d7534fece9978f081059fba
GorchPortley <admin@sdlabs.cc>
2024-11-30 14:26:58 -0500

    fixed pagination of drivers page so in order to cache correctly

--- resources/themes/blank/pages/drivers/index.blade.php
+++ resources/themes/blank/pages/drivers/index.blade.php
@@ -12,7 +12,6 @@ new class extends Component {
 
     public function with(): array
     {
-        $user = auth()->user()?->load('cart.items');
 
         return [
             'drivers' => Driver::query()
