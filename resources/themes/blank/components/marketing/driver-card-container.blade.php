1ad360f9a28ad823f039855060cc6b29879bcccb
GorchPortley <admin@sdlabs.cc>
2024-11-30 13:59:30 -0500

    fixed pagination of drivers page so in order to cache correctly

--- resources/themes/blank/components/marketing/driver-card-container.blade.php
+++ resources/themes/blank/components/marketing/driver-card-container.blade.php
@@ -1,4 +1,3 @@
-<div>
 <div class="grid grid-cols-1 lg:grid-cols-5 gap-6 p-4">
     @foreach($drivers as $driver)
         <div x-data="{ showSpecs: false }" wire:key="{{$driver->id}}" class="relative flex flex-row xs:flex-col lg:flex-col bg-white rounded-lg border-2 border-gray-100 hover:shadow-lg shadow-sm overflow-hidden">
@@ -78,5 +77,4 @@
             </x-button>
         </div>
     @endforeach
-</div>
 </div>
