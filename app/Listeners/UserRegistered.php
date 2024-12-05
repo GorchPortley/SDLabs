cc0e625246a59844e65b5d86a12d66e6b5cffd49
GorchPortley <admin@sdlabs.cc>
2024-12-05 17:18:06 -0500

    third attempt

--- app/Listeners/UserRegistered.php
+++ app/Listeners/UserRegistered.php
@@ -3,18 +3,7 @@
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Registered;
-use Maicol07\SSO\User;
 use Maicol07\SSO\Flarum;
-
-$flarum = new Flarum([
-    'url' => env('FORUM_URL'),
-    'root_domain' => env('APP_URL'),
-    'api_key' => env('FORUM_API_KEY'),
-    'password_token' => env('PASSWORD_TOKEN'),
-    'remember' => true,
-    'verify_ssl' => env('FORUM_VERIFY_SSL', true),
-    'cookies_prefix' => 'flarum',
-]);
 
 class UserRegistered
 {
@@ -25,14 +14,21 @@ class UserRegistered
     /**
      * Handle the event.
      */
-    public function handle($flarum, Registered $event): void
+    public function handle(Registered $event): void
     {
-        $user = $event->user;
-        $user = $flarum->user($user->name);
-        $flarum_user = $flarum->user();
-        $flarum_user->attributes->email = $user->email;
-        $flarum_user->attributes->password = $user->password;
-        $flarum_user->signup();
-        $flarum_user->login();
+        $flarum = new Flarum([
+            'url' => env('FORUM_URL'),
+            'root_domain' => env('APP_URL'),
+            'api_key' => env('FORUM_API_KEY'),
+            'password_token' => env('PASSWORD_TOKEN'),
+            'remember' => true,
+            'verify_ssl' => env('FORUM_VERIFY_SSL', true),
+            'cookies_prefix' => 'flarum',
+        ]);
+
+        $user = $flarum->user($event->user->getAuthIdentifier());
+        $user->attributes->password = $event->user->getAuthPassword();
+        $user->signup();
+        $user->login();
     }
 }
