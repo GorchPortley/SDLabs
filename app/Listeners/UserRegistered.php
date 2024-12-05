6452caed372e0c0200809937ffeda9605884f3c9
GorchPortley <admin@sdlabs.cc>
2024-12-05 17:43:27 -0500

    third attempt

--- app/Listeners/UserRegistered.php
+++ app/Listeners/UserRegistered.php
@@ -3,12 +3,12 @@
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Registered;
-use Maicol07\SSO\Flarum;
 
 class UserRegistered
 {
     public function __construct()
     {
+
     }
 
     /**
@@ -16,19 +16,5 @@ class UserRegistered
      */
     public function handle(Registered $event): void
     {
-        $flarum = new Flarum([
-            'url' => env('FORUM_URL'),
-            'root_domain' => env('APP_URL'),
-            'api_key' => env('FORUM_API_KEY'),
-            'password_token' => env('PASSWORD_TOKEN'),
-            'remember' => true,
-            'verify_ssl' => env('FORUM_VERIFY_SSL', true),
-            'cookies_prefix' => 'flarum',
-        ]);
-
-        $user = $flarum->user($event->user->getAuthIdentifier());
-        $user->attributes->password = $event->user->getAuthPassword();
-        $user->signup();
-        $user->login();
     }
 }
