3df6f44c6fb868b8dc8ef3fca8e7a996161de720
GorchPortley <admin@sdlabs.cc>
2024-12-11 02:21:27 -0500

    clean up

--- resources/themes/blank/pages/dashboard/index.blade.php
+++ resources/themes/blank/pages/dashboard/index.blade.php
@@ -6,8 +6,12 @@
 
 <x-layouts.app>
     @volt('dashboard')
-
-	<h1>This will show a lot of cool things i think</h1>
-
+<div>
+	<h1>Welcome to the SDLabs.cc Beta!</h1>
+<p>If you have questions, please head to the Soap Box to read an introduction and some walkthroughs about what SDLabs.cc is about!</p>
+    <p>feel free to email Adrian at admin@sdlabs.cc for any further questions or discussions!</p>
+    <x-button tag="a" href="https://www.sdlabs.cc/blog/site-news/welcome-to-the-sdlabscc-beta">Read the introduction here</x-button>
+    <x-button tag="a" href="https://www.sdlabs.cc/blog/">Browse the SoapBox!</x-button>
+</div>
     @endvolt
 </x-layouts.app>
