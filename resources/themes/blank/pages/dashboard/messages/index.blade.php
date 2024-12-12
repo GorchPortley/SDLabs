f4fbbd7c471261d18112fff2c37a518afa0431f4
GorchPortley <admin@sdlabs.cc>
2024-12-12 14:52:50 -0500

    Added chat functionality and placed in dashboard. run php artisan chatify:install and migrate etc

---
+++ resources/themes/blank/pages/dashboard/messages/index.blade.php
@@ -0,0 +1,19 @@
+<?php
+
+use function Laravel\Folio\{middleware, name};
+
+middleware('auth');
+name('dashboard.messages');
+?>
+
+<x-layouts.app>
+    <x-app.heading
+        title="Chat and Messages"
+        description="Connect with fellow SDLabs users">
+    </x-app.heading>
+    @volt('dashboard.files')
+    <iframe src="/chatify"
+            class="rounded-lg"
+            style="width: 100%; height:800px; overflow: hidden;"></iframe>
+    @endvolt
+</x-layouts.app>
