584da949e3c250d0ce9c1e299d4c752f9ab2e835
GorchPortley <admin@sdlabs.cc>
2024-12-12 23:26:09 -0500

    fixed sso for good hopefully

--- app/Listeners/UserLoggedIn.php
+++ app/Listeners/UserLoggedIn.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Login;
 use Maicol07\SSO\Flarum;
+use Illuminate\Support\Facades\Log;
 
 class UserLoggedIn
 {
@@ -11,22 +11,24 @@ class UserLoggedIn
     {
         $user = $event->user;
 
+        try {
             $flarum = new Flarum([
                 'url' => env('FORUM_URL'),
                 'root_domain' => env('APP_URL'),
                 'api_key' => env('FORUM_API_KEY'),
                 'password_token' => env('FORUM_PASSWORD_TOKEN'),
                 'remember' => true,
-                'verify_ssl' => env('FORUM_VERIFY_SSL', true),
+                'verify_ssl' => env('FORUM_VERIFY_SSL'),
             ]);
 
             // Create user in Flarum
-            $flarum_user = $flarum->user($user->email);
+            $flarum_user = $flarum->user($user->id);
             $flarum_user->attributes->username = $user->username ?? $user->name;
             $flarum_user->attributes->email = $user->email;
             $flarum_user->attributes->password = $user->password;
-
-            $flarum_user->signup();
             $flarum_user->login();
+        } catch (\Exception $e) {
+            Log::error('Flarum SSO Login Error: ' . $e->getMessage());
+        }
     }
 }
